3584dd31a4ad2d5ec8b5f8a980e0414d2c9e3e47
Neil Dino <N.dino90@gmail.com>
2022-01-05 21:45:27 -0700

    small change to reviews

--- studio/schemas/reviewInfo.js
+++ studio/schemas/reviewInfo.js
@@ -2,6 +2,7 @@ export default {
     type: 'object',
     name: 'reviewInfo',
     fields: [
+        { name: 'name', type: 'string', title: 'Reviewer Name' },
         { name: 'reviewImage', type: 'image', title: 'Review Image' },
         { name: 'reviewText', type: 'string', title: 'Review Text' }
     ]
